6c2cdf77afe2d2e27f1938b3b4a8bbd73b5497b8
Erison Silva <hey@erison.work>
2025-08-22 11:43:58 +0200

    Fixed colorschema config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,7 @@
 return {
   { -- plugin spec for catppuccin
     "catppuccin/nvim",
-  },
-
-  -- this belongs to no plugin spec and is ignored
-  lazy = false,
-  name = "catppuccin",
-  opts = {
-    transparent_background = true,
-  },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
+    name = "catppuccin",
+    priority = 1000,
   },
 }
